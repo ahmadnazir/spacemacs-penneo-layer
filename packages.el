ab8f7b68655731144b074f10e510056de1036310
Ahmad N. Raja <ar@penneo.com>
2017-04-24 23:59:54 +0200

    Added penneo services (using prodigy)

--- packages.el
+++ packages.el
@@ -2,7 +2,7 @@
 ;;
 ;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
 ;;
-;; Author: Ahmad Nazir Raja <mandark@mandark>
+;; Author: Ahmad Nazir Raja <ahmadnazir@gmail.com>
 ;; URL: https://github.com/syl20bnr/spacemacs
 ;;
 ;; This file is not part of GNU Emacs.
@@ -32,6 +32,7 @@
 (defconst penneo-packages
   '(
     (penneo-auth :location local)
+    (prodigy)
     )
   "The list of Lisp packages required by the penneo layer.
 
@@ -62,7 +63,67 @@ Each entry is either:
 
 (defun penneo/init-penneo-auth ()
   "Initialize penneo-auth package"
-  (use-package penneo-auth)
+  (use-package penneo-auth))
+
+(defun penneo/post-init-prodigy()
+  "Initialize window package"
+
+  (prodigy-define-service
+    :name "gateway"
+    :command (concat penneo-code-dir "gateway-service/project-runner/run.sh")
+    :args '("dev")
+    :stop-signal 'sigkill
+    :kill-process-buffer-on-stop nil)
+
+  (prodigy-define-service
+    :name "frontend"
+    :command "npm"
+    :args '("run" "start")
+    :cwd (concat penneo-code-dir "fe-application-loader")
+    :kill-process-buffer-on-stop nil)
+
+  (prodigy-define-service
+    :name "auth"
+    :command (concat penneo-code-dir "api-auth/project-runner/run.sh")
+    :args '("dev")
+    :stop-signal 'sigkill
+    :kill-process-buffer-on-stop nil)
+
+  (prodigy-define-service
+    :name "sign"
+    :command (concat penneo-code-dir "Symfony2/project-runner/run.sh")
+    :args '("dev")
+    :stop-signal 'sigkill
+    :kill-process-buffer-on-stop nil)
+
+  (prodigy-define-service
+    :name "workflows/forms"
+    :command (concat penneo-code-dir "forms/project-runner/run.sh")
+    :args '("dev")
+    :stop-signal 'sigkill
+    :kill-process-buffer-on-stop nil)
+
+  (prodigy-define-service
+    :name "pdf / eid"
+    :command (concat penneo-code-dir "pdf-eid/project-runner/run.sh")
+    :args '("dev")
+    :stop-signal 'sigkill
+    :kill-process-buffer-on-stop nil)
+
+  (prodigy-define-service
+    :name "validator"
+    :command (concat penneo-code-dir "validator/project-runner/run.sh")
+    :args '("dev")
+    :stop-signal 'sigkill
+    :kill-process-buffer-on-stop nil)
+
+  (prodigy-define-service
+    :name "sepior"
+    :command (concat penneo-code-dir "SepiorService/project-runner/run.sh")
+    :args '("dev")
+    :tags '(java)
+    :stop-signal 'sigkill
+    :kill-process-buffer-on-stop nil)
   )
 
 ;;; packages.el ends here
